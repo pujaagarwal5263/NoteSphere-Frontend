56cadcdb2222b2a7bbc19381a8f24685fe56f9ac
geekpuja <pujuagarwal5263@gmail.com>
2023-08-22 23:49:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import Home from "../src/components/Home";
+import Form from "./components/Form";
+import MyNotes from "./components/MyNotes";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/event" element={<Event />} /> */}
+        {/* <Route path="/profile" element={<Dashboard />} /> */}
+        <Route path="/form" element={<Form />} />
+        <Route path="/MyNotes" element={<MyNotes />} />
+        <Route />
+      </Routes>
+    </ChakraProvider>
+  );
+}
+
+export default App;
